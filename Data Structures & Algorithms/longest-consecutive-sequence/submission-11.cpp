96d8b082e589e75d48eab951bfb8f6d87839f02c
Ethan Howes <134120826+EAHowes@users.noreply.github.com>
2026-07-17 13:21:08 -0400

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> fmap;
+        for (int i = 0; i <nums.size(); ++i) {
+            fmap[nums[i]]++;
+        }
+
+        int longest = 0;
+
+        for (int i = 0; i <nums.size(); ++i) {
+            if (fmap.count(nums[i] -1) == 0) {
+            int currlen =1;
+            int curval = nums[i];
+            while (fmap.count(curval + 1) != 0) {
+                curval ++;
+                currlen ++;
+            }
+
+            if (currlen > longest) longest = currlen;
+        }}
+
+        return longest;
+    }
+};
