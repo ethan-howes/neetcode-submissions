3a478b10c4937e42add7e9324153cb4d04849ef1
Ethan Howes <134120826+ethan-howes@users.noreply.github.com>
2026-07-29 12:12:53 -0400

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int max = 0;
+        int currheight = 0;
+        int currmax = 0;
+        int width = 0;
+        for (int i = 0; i < heights.size(); ++i) {
+            currheight = heights[i];
+            currmax = 0;
+            width = 0;
+            for (int j = i+1; j < heights.size(); ++j) {
+                if (heights[j] < currheight) {
+                    currheight = heights[j];
+                } else if (heights[j] == 0) {
+                    currmax = currheight * (j - i);
+                    break;
+                }
+                if (currmax < (currheight * (j - i +1))) currmax = (currheight * (j - i +1));
+            }
+            if (currmax > max) {
+                max = currmax;
+            }
+            if (currheight > max) max = currheight;
+
+        }
+        return max;
+    }
+};
---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> fmap;
+        for (int i = 0; i <nums.size(); ++i) {
+            fmap[nums[i]]++;
+        }
+
+        int longest = 0;
+        if (nums.size() > 0) longest++;
+
+        for (int i = 0; i <nums.size(); ++i) {
+            int currlen =0;
+            int curval = nums[i];
+            while (fmap.count(curval + 1) != 0) {
+                curval ++;
+                currlen ++;
+            }
+
+            if (currlen > longest) longest = currlen+ 1;
+        }
+
+        return longest;
+    }
+};
